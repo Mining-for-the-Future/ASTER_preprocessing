ba319b689f7b687b1d574278299663077c4677f3
Eli Weaverdyck <81333200+eweaverdyck@users.noreply.github.com>
2024-02-06 18:22:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author_email = 'eweaverdyck@gmail.com',
     license = 'MIT',
     packages = ['ASTER_preprocessing'],
-    install_requires = ['earth-engine-api']
+    install_requires = ['earthengine-api']
 )
