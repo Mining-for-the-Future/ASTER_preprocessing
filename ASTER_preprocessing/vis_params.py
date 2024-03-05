71b6fcc004c9bafd9abbf50aff5f115ed4764ecb
Eli Weaverdyck <81333200+eweaverdyck@users.noreply.github.com>
2024-03-05 10:59:17 -0600

    bestEffort

--- ASTER_preprocessing/vis_params.py
+++ ASTER_preprocessing/vis_params.py
@@ -4,7 +4,12 @@ def vis_params(img_dict, geometry, bands):
     """
     Returns a dictionary of visualization parameters for ASTER imagery.
     """
-    percentiles = img_dict['imagery'].select(bands).reduceRegion(ee_i.Reducer.percentile([5, 95]), geometry, crs = img_dict['crs'], crsTransform = img_dict['transform']).getInfo()
+    percentiles = img_dict['imagery'].select(bands).reduceRegion(
+        reducer =ee_i.Reducer.percentile([5, 95]), 
+        geometry = geometry, 
+        crs = img_dict['crs'], 
+        crsTransform = img_dict['transform'],
+        bestEffort = True).getInfo()
     b_p5 = [band + '_p5' for band in bands]
     b_p95 = [band + '_p95' for band in bands]
     mins = [percentiles[min] for min in b_p5]
