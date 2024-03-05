82ae25e4b5cabc4398dd0445e300befeb6c4e072
Eli Weaverdyck <81333200+eweaverdyck@users.noreply.github.com>
2024-03-05 10:52:17 -0600

    module vis_params
    created a module that will calculate visualization parameters from an img_dict (returned by ASTER_preprocessing.preprocessing.aster_preprocessing) using the percent clip method.

---
+++ ASTER_preprocessing/vis_params.py
@@ -0,0 +1,13 @@
+from .preprocessing import ee_i
+
+def vis_params(img_dict, geometry, bands):
+    """
+    Returns a dictionary of visualization parameters for ASTER imagery.
+    """
+    percentiles = img_dict['imagery'].select(bands).reduceRegion(ee_i.Reducer.percentile([5, 95]), geometry, crs = img_dict['crs'], crsTransform = img_dict['transform']).getInfo()
+    b_p5 = [band + '_p5' for band in bands]
+    b_p95 = [band + '_p95' for band in bands]
+    mins = [percentiles[min] for min in b_p5]
+    maxs = [percentiles[max] for max in b_p95]
+    vis_params = {'min': mins, 'max': maxs, 'bands': bands}
+    return vis_params
